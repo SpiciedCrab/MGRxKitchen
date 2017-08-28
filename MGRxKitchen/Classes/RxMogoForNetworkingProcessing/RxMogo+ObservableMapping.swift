acbc636e9c41e2814aa8237e372d1c4a5125841f
Harly <Harly@SpiciedCrab.local>
2017-08-28 13:58:41 +0800

    f

--- MGRxKitchen/Classes/RxMogoForNetworkingProcessing/RxMogo+ObservableMapping.swift
+++ MGRxKitchen/Classes/RxMogoForNetworkingProcessing/RxMogo+ObservableMapping.swift
@@ -12,14 +12,32 @@ import RxCocoa
 import RxSwift
 import HandyJSON
 
-// MARK: - Observable model转化
-public extension Observable where Element == MGResponse
+// MARK: - Observable json转化
+public extension Observable where Element == Dictionary<String , Any>
 {
     /// 把你的Response变成model啦
     ///
     /// - Returns: 砖头
     func mapBricks<ModelElement : HandyJSON>() -> Observable<ModelElement>
     {
-        return map { ModelElement.deserialize(from: $0.content as NSDictionary) ?? ModelElement() }
+        return map { ModelElement.deserialize(from: $0 as NSDictionary) ?? ModelElement() }
+    }
+}
+
+// MARK: - Observable array转化
+public extension Observable where Element == Array<[String : Any]>
+{
+    /// 把你的[Json]变成[model]啦
+    ///
+    /// - Returns: 砖头
+    func mapBricks<ModelElement : HandyJSON>() -> Observable<[ModelElement]>
+    {
+        
+        return map { (arrJson)  in
+            if let models =  [ModelElement].deserialize(from: arrJson as NSArray ) as? [ModelElement]{
+                return models
+            }
+            return [ModelElement]()
+        }
     }
 }
